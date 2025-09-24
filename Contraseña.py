9c1c2531f7f9d4036794ee30f1be05ca8c3f00a4
Arsd1243 <armando.a.j.sanchez.depaz@gmail.com>
2025-09-24 17:39:52 -0500

    Create Contraseña.py
    creador de contraseñas

---
+++ Contraseña.py
@@ -0,0 +1,10 @@
+import random
+generator = "☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕‼¶§▬↨↑↓→←∟↔▲▼ !#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ ÇüéâäàåçêëèïîìÄÅÉæÆôöòûùÿÖÜ¢£¥₧ƒáíóúñÑªº¿⌐¬½¼¡«»░▒▓│┤╡╢╖╕╣║╗╝╜╛┐└┴┬├─┼╞╟╚╔╩╦╠═╬╧╨╤╥╙╘╒╓╫╪┘┌█▄▌▐▀αßΓπΣσµτΦΘΩδ∞φε∩≡±≥≤⌠⌡÷≈°∙·√ⁿ²■"
+
+longitud = int(input("Introduce la longitud de la contraseña:    "))
+
+contraseña = ""
+
+for i in range(longitud):
+    contraseña += random.choice(generator)
+print("Tu contraseña es:  ", contraseña)
